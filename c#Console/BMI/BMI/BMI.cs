f0cdda91eccb36de1104f09f118e138be9a6560e
Stephen Britt <sdbritt@senators.ws.edu>
2023-04-21 21:40:50 -0400

    Finished the console application for imperial units

--- c#Console/BMI/BMI/BMI.cs
+++ c#Console/BMI/BMI/BMI.cs
@@ -5,10 +5,66 @@ using System.Text;
 using System.Threading.Tasks;
 
 //Edited by: Stephen Britt
-//     Date: 2/6/2022
+//     Date: 4/21/2023
 
 class BMI {
-    // member varibles
+    // member variables
     private double m_userWeightInPounds;
     private double m_userHeightInInches;
+
+    public double UserWeightInPounds {
+        get {
+            return m_userWeightInPounds;
+        } // end get
+
+        set {
+            if (value > 0) {
+                m_userWeightInPounds = value;
+            } else {
+                throw new Exception("A weight must be greater than zero.");
+            } // end if
+        } // end set
+    } // end property
+
+    public double UserHeightInInches {
+        get {
+            return m_userHeightInInches;
+        } // end get
+
+        set {
+            if (value > 0) {
+                m_userHeightInInches = value;
+            } else {
+                throw new Exception("A height must be greater than zero.");
+            } // end if
+        } // end set
+    } // end property
+
+    public BMI(double weightInPounds = 1, double heightInInches = 1) {
+        UserWeightInPounds= weightInPounds;
+        UserHeightInInches= heightInInches;
+    } // end constructor
+
+    public double GetBMIScore() {
+        double bmiScore = (UserWeightInPounds * 703) / (UserHeightInInches * UserHeightInInches);
+        return bmiScore;
+    } // end method
+
+    public string GetBMIValue() {
+        if (GetBMIScore() < 18.5) {
+            return "underweight";
+        } else if (GetBMIScore() >= 18.5 && GetBMIScore() <= 24.9) {
+            return "normal";
+        } else if (GetBMIScore() >= 25 &&  GetBMIScore() <= 29.9) {
+            return "overweight";
+        } else {
+            return "obese";
+        } // end if
+    } // end method
+
+    public override string ToString() {
+        return $"For a person of {UserWeightInPounds:f1} lbs. and " +
+            $"{UserHeightInInches:f1} in. the general BMI score is {GetBMIScore():f1}.\n" +
+            $"The National Institute of Health estimates this to be {GetBMIValue()}.";
+    } // end method
 } // end class
--- c#Console/BMI/BMI/BMITest.cs
+++ c#Console/BMI/BMI/BMITest.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 
 //Edited by: Stephen Britt
-//     Date: 2/6/2022
+//     Date: 4/21/2023
 
 class BMITest {
     static void Main() {
-        Console.WriteLine("This program calculates your BMI and informs you of your weight category.");        
+        Console.WriteLine("This program calculates your BMI and informs you of your weight category.");
+
+        BMI bmi = new BMI();
+
+        while (true) {
+            try {
+                Console.Write("\nPlease enter your weight in pounds: ");
+                bmi.UserWeightInPounds = double.Parse(Console.ReadLine());
+
+                break;
+            } catch {
+                Console.WriteLine("\nA user weight must be a positive number.");
+            } // end try
+        } // end while
+
+        while (true) {
+            try {
+                Console.Write("\nPlease enter your height in inches: ");
+                bmi.UserHeightInInches = double.Parse(Console.ReadLine());
+
+                break;
+            } catch {
+                Console.WriteLine("\nA user height must be a positive number.");
+            } // end try
+        } // end while
+
+        Console.WriteLine($"\n{bmi}");
     } // end method
 } // end class
