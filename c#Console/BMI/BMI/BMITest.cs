d73c051efd1a0e45c75adc6ea8c79cabd4c826ca
Stephen Britt <sdbritt@senators.ws.edu>
2023-04-21 21:50:42 -0400

    Updated the file to ask if the user wants to do multiple calculations

--- c#Console/BMI/BMI/BMITest.cs
+++ c#Console/BMI/BMI/BMITest.cs
@@ -7,30 +7,44 @@ class BMITest {
     static void Main() {
         Console.WriteLine("This program calculates your BMI and informs you of your weight category.");
 
-        BMI bmi = new BMI();
+        string repeat = "Y";
 
-        while (true) {
-            try {
-                Console.Write("\nPlease enter your weight in pounds: ");
-                bmi.UserWeightInPounds = double.Parse(Console.ReadLine());
+        while (repeat.ToUpper() == "Y") {
+            BMI bmi = new BMI();
 
-                break;
-            } catch {
-                Console.WriteLine("\nA user weight must be a positive number.");
-            } // end try
+            while (true) {
+                try {
+                    Console.Write("\nPlease enter your weight in pounds: ");
+                    bmi.UserWeightInPounds = double.Parse(Console.ReadLine());
+
+                    break;
+                } catch {
+                    Console.WriteLine("\nA user weight must be a positive number.");
+                } // end try
+            } // end while
+
+            while (true) {
+                try {
+                    Console.Write("\nPlease enter your height in inches: ");
+                    bmi.UserHeightInInches = double.Parse(Console.ReadLine());
+
+                    break;
+                } catch {
+                    Console.WriteLine("\nA user height must be a positive number.");
+                } // end try
+            } // end while
+
+            Console.WriteLine($"\n{bmi}");
+
+            Console.Write("\nWould you like to calculate another person's BMI? (Y/n): ");
+            repeat = Console.ReadLine();
+
+            while (!(repeat.ToUpper() == "Y" || repeat.ToUpper() == "N")) {
+                Console.WriteLine("\nPlease enter 'Y' to continue or 'N' to quit.");
+
+                Console.Write("\nWould you like to calculate another person's BMI? (Y/n): ");
+                repeat = Console.ReadLine();
+            } // end while
         } // end while
-
-        while (true) {
-            try {
-                Console.Write("\nPlease enter your height in inches: ");
-                bmi.UserHeightInInches = double.Parse(Console.ReadLine());
-
-                break;
-            } catch {
-                Console.WriteLine("\nA user height must be a positive number.");
-            } // end try
-        } // end while
-
-        Console.WriteLine($"\n{bmi}");
     } // end method
 } // end class
