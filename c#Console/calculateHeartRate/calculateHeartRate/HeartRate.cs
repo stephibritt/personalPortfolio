9aa3162debcedcb4ca7ca4a266fc25564dcb5f6b
Stephen Britt <sdbritt@senators.ws.edu>
2023-04-24 01:23:39 -0400

    Updated files

---
+++ c#Console/calculateHeartRate/calculateHeartRate/HeartRate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+//Edited by: Stephen Britt
+//     date: 2/02/2022
+
+class HeartRate {
+
+    // member variables
+    private string m_name;
+    private int m_birthYear;
+
+    // name property
+    public string Name {
+        get {
+            return m_name;
+        } // end get
+
+        set {
+            if (m_name == string.Empty) {
+                throw new Exception("A name requires one or more characters.");
+            } else {
+                m_name = value;
+            } // end if
+        } // end set
+    } // end property
+
+    // birth year property
+    public int BirthYear {
+        get {
+            return m_birthYear;
+        } // end get
+        set {
+            if (value < (DateTime.Now.Year - 125) || value > DateTime.Now.Year - 1) {
+                throw new Exception($"Invalid birth year. Year must be between {DateTime.Now.Year - 125} and {DateTime.Now.Year - 1}.");
+            } else {
+                m_birthYear = value;
+            } // end if
+        } // end set
+    } // end property
+
+    // public methods
+    public HeartRate(string name, int birthYear) {
+        this.Name = name;
+        this.BirthYear = birthYear; 
+    } // end constructor
+
+    public int GetAge() {
+        return DateTime.Now.Year - BirthYear;
+    } // end method
+
+    public int GetMaxHeartRate() {        
+        return 220 - GetAge();
+    } // end method
+
+    public double GetMinTargetHeartRate() {
+        return GetMaxHeartRate() * .50;
+    } // end method
+
+    public double GetMaxTargetHeartRate() {
+        return GetMaxHeartRate() * .85;
+    } // end method
+} // end class
---
+++ c#Console/calculateHeartRate/calculateHeartRate/HeartRateTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+//Edited by: Stephen Britt
+//     date: 2/02/2022
+
+class HeartRateTest {
+    static void Main() {
+
+        // output program purpose
+        Console.WriteLine("Target Heart-Rate Calculator");
+        Console.WriteLine("\nWARNING: This calculation should not be used to make medical decisions.\n" +
+            "Always consult a physician or qualified health professional before beginning or modifying an exercise program.");
+        Console.WriteLine("\nAnswer the following questions to determine your safe heart rate for exercising.");
+
+
+        // User input for name and year of birth
+        Console.Write("\nWhat is your name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your birth year? ");
+        int birthYear = int.Parse(Console.ReadLine());
+
+        // instantiate the class
+        HeartRate userHeartRate = new HeartRate(name, birthYear);
+
+        // output calculations
+        Console.WriteLine($"\nHeart rate information for {userHeartRate.Name}:");
+        Console.WriteLine($"    Age: {userHeartRate.GetAge(), 24} years old");
+        Console.WriteLine($"    Maximum heart rate: {userHeartRate.GetMaxHeartRate(), 12:f1} beats/minute");
+        Console.WriteLine($"    Minimum target heart rate: {userHeartRate.GetMinTargetHeartRate():f1} beats/minute");
+        Console.WriteLine($"    Maximum target heart rate: {userHeartRate.GetMaxTargetHeartRate():f1} beats/minute");
+    } // end method
+} // end class
