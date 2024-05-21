84b872e5e68cb5a233e8826d8e54c3016836a261
Stephen Britt <sdbritt@senators.ws.edu>
2024-05-21 06:04:46 -0400

    Added a check for root access to determineHackTarget.js

--- javascript/bitburner/determineHackTarget.js
+++ javascript/bitburner/determineHackTarget.js
@@ -18,18 +18,21 @@ export async function main(ns) {
     let potentialTarget = hackHosts[i];
     let serverHackLevel = ns.getServerRequiredHackingLevel(potentialTarget);
 
-    // define the sentinel values to choose a server
-    // the server will be chosen if the hack level is
-    // lower than the players hack level and higher than the hack
-    // level sentinel. This will ensure that the chosen target is 
-    // viable to hack and as close to the player level as possible
+    /* define the sentinel values to choose a server
+    the server will be chosen if the hack level is
+    lower than the players hack level and higher than the hack
+    level sentinel. This will ensure that the chosen target is 
+    viable to hack and as close to the player level as possible. 
+    Must have root access as well*/
 
-    if ((serverHackLevel <= playerHackingLevel) && (serverHackLevel >= hackLevelSentinel)) {
+
+    if ((serverHackLevel <= playerHackingLevel) && (serverHackLevel >= hackLevelSentinel) &&
+      ns.hasRootAccess(potentialTarget)) {
       hackLevelSentinel = serverHackLevel;
       hackTarget = potentialTarget;
     }
   }
-  
+
   // send the hack target to a port
   ns.clearPort(portNumber)
   ns.writePort(portNumber, hackTarget);
