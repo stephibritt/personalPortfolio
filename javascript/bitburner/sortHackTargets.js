e35eb8a8242733987b5b13780114e87cd46eeb96
Stephen Britt <sdbritt@senators.ws.edu>
2024-06-04 06:30:39 -0400

    Removed unused files
    Renamed codifyHackLevels.js to sortHackTargets.js to make the
    name more descriptive about the function of the script
    
    Factored out the sorting logic of sortHackTargets.js into a function
    with intent to add a choice on how to sort the targets
    Sort by money currently not efficient enough

--- javascript/bitburner/sortHackTargets.js
+++ javascript/bitburner/sortHackTargets.js
@@ -1,5 +1,21 @@
 /** @param {NS} ns */
 export async function main(ns) {
+  let sortMode = "Hacking Level";
+
+  //sortMode = await ns.prompt("Please choose how to sort the list of servers:", { type: "select", choices: ["Hacking Level", "Max Money"] });
+
+  ns.run("sanitizeHackHosts.js");
+  
+  if (sortMode == "Max Money") {
+    let targetsSorted = await sortByMoney(ns);
+    ns.write("targetsSorted.txt", targetsSorted.toString().replaceAll(",", "\n"), "w");
+  } else {
+    let targetsSorted = await sortByLevel(ns);
+    ns.write("targetsSorted.txt", targetsSorted.toString().replaceAll(",", "\n"), "w");
+  }
+}
+
+async function sortByLevel(ns) {
   let hackTargets = ns.read("hackHosts.txt").split(", ");
   let targetsAndLevel = [];
 
@@ -18,7 +34,6 @@ export async function main(ns) {
   while (levelThreshold < 3000) {
     for (let i = 0; i < targetsAndLevel.length; i++) {
       let currentTarget = targetsAndLevel[i];
-      //let targetInfo = "Server Name: " + currentTarget.name + "         Min. Hacking Level: " + currentTarget.level;
       let targetInfo = `Server Name: ${currentTarget.name}`.padding(35) + `Min. Hack Level: ${currentTarget.level}`;
 
       if (targetsAndLevelSorted.includes(targetInfo)) {
@@ -32,7 +47,43 @@ export async function main(ns) {
     levelThreshold++;
   }
 
-  ns.write("targetsAndLevel.txt", targetsAndLevelSorted.toString(), "w");
+  return targetsAndLevelSorted;
+}
+
+async function sortByMoney(ns) {
+  let hackTargets = ns.read("hackHosts.txt").split(", ");
+  let targetsAndMoney = [];
+
+  for (let i = 0; i < hackTargets.length; i++) {
+    let currentTarget = hackTargets[i];
+
+    let currentTargetMaxMoney = ns.getServerMaxMoney(currentTarget);
+
+    let targetToAdd = { name: currentTarget, maxMoney: currentTargetMaxMoney };
+
+    targetsAndMoney.push(targetToAdd)
+  }
+
+  let targetsAndMoneySorted = [];
+  let moneyThreshold = 1;
+  //100000000000000
+  while (moneyThreshold < 1000000000000) {
+    for (let i = 0; i < targetsAndMoney.length; i++) {
+      let currentTarget = targetsAndMoney[i];
+      let targetInfo = `Server Name: ${currentTarget.name}`.padding(35) + `Max Money: ${currentTarget.maxMoney}`;
+
+      if (targetsAndMoneySorted.includes(targetInfo)) {
+        continue;
+      }
+
+      if (currentTarget.maxMoney <= moneyThreshold) {
+        targetsAndMoneySorted.push(targetInfo);
+      }
+    }
+    moneyThreshold *= 10;
+  }
+
+  return targetsAndMoneySorted;
 }
 
 /**
@@ -55,6 +106,6 @@ String.prototype.padding = function (padding, paddingCharacter) {
   let maxLength = Math.max((Math.abs(padding) - this.length) || 0, 0);
 
   let pad = Array(maxLength + 1).join(String(paddingCharacter || ' ').charAt(0));
-  
+
   return (padding < 0) ? pad + inputString : inputString + pad;
 };
