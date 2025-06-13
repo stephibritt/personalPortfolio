0e93261937ded95259f642f26b6b8ba2522a3b46
Stephen Britt <sdbritt@senators.ws.edu>
2025-06-13 03:41:35 -0400

    Improved error handling and added some try blocks to handle known issues with empty except blocks since they are not pertinent to the program

--- python/image viewer/image_viewer.py
+++ python/image viewer/image_viewer.py
@@ -1,6 +1,7 @@
 import tkinter
 from tkinter import filedialog
 import os
+import PIL
 from PIL import Image, ImageTk, ImageGrab
 
 def Main():
@@ -11,8 +12,16 @@ class ImageViewer:
     def __init__(self) -> None:
         # create the main window
         self.mainWindow = tkinter.Tk()
-        self.mainWindow.iconbitmap("image_viewer.ico")
-        self.mainWindow.title("Image Viewer")
+
+        # for some reason this throws an error in debug mode. I am putting it in a try loop to mitigate that. It works when run via the batch file.
+        try:
+            self.mainWindow.iconbitmap("image_viewer.ico")
+        except:
+            # do nothing
+            pass
+        # end try
+        
+        self.mainWindow.title("ImV")
         self.mainWindow.attributes("-topmost", True)
 
         # create the frames
@@ -27,30 +36,24 @@ class ImageViewer:
 
         self.onTopBtn.pack(side="left")
         self.chooseImgBtn.pack(side="left")
-        
-        img = self.getImage()
-        self.imageArea = tkinter.Label(self.imageFrame, image=img)
-        self.imageArea.pack()
 
         self.buttonsFrame.pack()
-        self.imageFrame.pack()
-
-        tkinter.mainloop()
+        
+        # populate the image area
+        self.setImage()        
     # end func
 
     def changeImage(self):
         # forget the previous image
-        self.imageArea.pack_forget()
+        try:
+            self.imageArea.forget()
+        except:
+            # do nothing if it fails, this likely means an image was never packed
+            pass
+        # end try
 
         # select a new image and repack the frame
-        img = self.getImage()
-        self.imageArea = tkinter.Label(self.imageFrame, image=img)
-        self.imageArea.pack()
-
-        self.buttonsFrame.pack()
-        self.imageFrame.pack()
-
-        tkinter.mainloop()
+        self.setImage()
     # end func
     
     def toggleOnTop(self):
@@ -66,27 +69,53 @@ class ImageViewer:
         screenshotDirectory = "~/Pictures/Screenshots"
         
         file_path = filedialog.askopenfilename(title="Image To Show", initialdir=screenshotDirectory)
+
+        img = None
         
         if os.path.isdir(file_path) or len(file_path)==0:
             try:
                 img = ImageTk.PhotoImage(ImageGrab.grabclipboard())
             except KeyError:
-                print("KeyError, no key supplied to reference with. Quitting...")
-                quit()
+                print("KeyError: No key supplied to reference with. Returning None.")
             except AttributeError:
-                print("AttributeError, unable to use clipboard data. Quitting...")
-                quit()
+                print("AttributeError: Unable to use clipboard data. Returning None.")
+            except ValueError:
+                print("ValueError: Nothing chosen and no image in clipboard. Returning None.")
+            except:
+                print("Unknown Error: An unknown error occurred. Returning None.")
             # end try
         else:
             try:
                 img = ImageTk.PhotoImage(Image.open(file_path))
             except PermissionError:
-                print("PermissionError, unable to access media. Quitting...")
-                quit()
+                print("PermissionError: Unable to access media. Returning None.")
+            except PIL.UnidentifiedImageError: 
+                print("PIL.UnidentifiedImageError: Cannot identify image file. Returning None.")
+            except:
+                print("Unknown Error: An unknown error occurred. Returning None.")
             # end try
         # end if
         
         return img
+    # end func
+
+    def setImage(self):
+        # select a new image and repack the frame
+        img = self.getImage()
+
+        # either assign the image or if it returns as None, label it as no image selected.
+        if img is None:
+            self.imageArea = tkinter.Label(self.imageFrame, text="No image selected.", fg="black")
+        else:
+            self.imageArea = tkinter.Label(self.imageFrame, image=img)
+        # end if
+
+        self.imageArea.pack()
+
+        self.imageFrame.pack()
+
+        # build the window
+        tkinter.mainloop()
     # end func
 # end class
             
